c31dcd2af4bed640cfdded95046a9dfb65b99884
Mstiekema <merstiekema@gmail.com>
2017-09-20 22:29:26 +0200

    Info pagina template gemaakt
    Bootstrap uitgecomment en een paar comments neergezet voor wat extra uitleg

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ var express = require('express')
 var path = require('path')
 var app = express()
 
+// Website defaults
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+// Webpages
 app.get('/', function (req, res) {
   res.render('index.html')
 })
--- static/basic.js
+++ static/basic.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
   console.log("The page has loaded!");
+  // Test om te zien of de pagina is geladen
 });
