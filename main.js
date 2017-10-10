7a8dbf0dacb9ba17617bc0e89f9a81bf7036adc6
Mstiekema <merstiekema@gmail.com>
2017-10-10 17:08:55 +0200

    Login toegevoegd -> Database / request

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
+var bodyParser = require('body-parser');
 var express = require('express')
+var mysql = require('mysql')
 var path = require('path')
 var app = express()
+
+// MySQL
+var conn = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'pws',
+	port: 3306
+});
+
+conn.connect(function(err) {
+	if(err) return console.log('Database connection error: ', err);
+	console.log('MySQL connection successful')
+});
 
 // Website defaults
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('views', path.join(__dirname, 'templates'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
@@ -22,7 +40,30 @@ app.get('/zoek/:zoek', function (req, res) {
 })
 
 app.get('/login', function (req, res) {
-  res.render('login.html')
+  res.render('login.html', {type: 'login'})
+})
+
+app.get('/login/new', function (req, res) {
+  res.render('login.html', {type: 'new'})
+})
+
+// Posts
+app.post('/login', function(req, res) {
+	conn.query('SELECT * FROM userinfo WHERE username = ?', req.body.username, function(err, result) {
+		if (err || result[0] == undefined) return res.status(400).send('Username and/or password is incorrect') 
+		if (req.body.password == result[0].password) {
+			res.status(200).send('Successfully logged in!')
+		}	else { 
+			res.status(400).send('Incorrect password')
+		}
+	})
+})
+
+app.post('/login/new', function(req, res) {
+	conn.query('INSERT INTO userinfo set ?', req.body, function(err, result) {
+    if(err) return res.status(400).send('Kan account niet aanmaken')
+    res.status(200).send('Account succesvol aangemaakt.')
+	})
 })
 
 app.all('*', function(req, res, next) {
--- static/login.js
+++ static/login.js
@@ -1,9 +1,40 @@
 $(document).ready(function() {
   $('#login').on('click', function(event) {
-    var usr = {
-      username: $("input[name='username']").val(),
-      password: $("input[name='password']").val()
-    }
-    console.log(usr)
+    $.ajax({
+      url: 'http://localhost:1000/login',
+      data: {
+        username: $("input[name='username']").val(),
+        password: $("input[name='password']").val()
+      },
+      type: 'POST',
+      success: function (data) {
+        console.log(data)
+      },
+      error: function (xhr, status, error) {
+        console.log(xhr.responseText)
+      }
+    });
+  });
+  
+  $('#newLogin').on('click', function(event) {
+    var p1 = $("input[name='newPassword']").val();
+    var p2 = $("input[name='confPassword']").val();
+    if(p1 != p2) return window.alert("Wachtwoorden komen niet overeen");
+    
+    $.ajax({
+      url: 'http://localhost:1000/login/new',
+      data: {
+        username: $("input[name='newUsername']").val(),
+        password: p1,
+        email: $("input[name='email']").val()
+      },
+      type: 'POST',
+      success: function (data) {
+        console.log(data)
+      },
+      error: function (xhr, status, error) {
+        console.log(xhr.responseText)
+      }
+    });
   });
 });
