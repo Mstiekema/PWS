96acc461ff43aaf7ecd72ebd7c8565b406eb4979
Mstiekema <merstiekema@gmail.com>
2017-11-07 18:05:22 +0100

    Zoek knop verbeterd

--- static/basic.js
+++ static/basic.js
@@ -6,10 +6,23 @@ $(document).ready(function() {
     var zoek = document.getElementById('zoek').value
     if (zoek == "jitze's heterosexualiteit") {
       window.location.href = "/jitze";
-    } else if (zoek.includes("zeep")) {
+    } else if (zoek.indexOf("zeep") != -1) {
       window.open('https://www.youtube.com/watch?v=t99CXCA4GFs');
     } else {
       window.location.href = "/zoek/" + zoek
+    }
+  });
+  
+  $("#zoek").keyup(function(ev) {
+    if (ev.which === 13) {
+      var zoek = document.getElementById('zoek').value
+      if (zoek == "jitze's heterosexualiteit") {
+        window.location.href = "/jitze";
+        } else if (zoek.indexOf("zeep") != -1) {
+        window.open('https://www.youtube.com/watch?v=t99CXCA4GFs');
+      } else {
+        window.location.href = "/zoek/" + zoek
+      }
     }
   });
 });
