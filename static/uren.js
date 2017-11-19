287cf49503fd35396e45804a5ba6f42c5059af1d
Mstiekema <merstiekema@gmail.com>
2017-11-19 14:28:34 +0100

    Info knop over chart toegevoegd

--- static/uren.js
+++ static/uren.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
   // Activeert de popover over het invoeren van informatie als je niet bent ingelogd
-  $('[data-toggle="popover"]').popover({
+  $('[id="infBtn"]').popover({
     trigger: 'hover',
     html: true,
     title: "Waarom kan ik niks doen?",
     content: "Hier kunnen alleen Jelte, Jitze en Merijn hun gegevens invoeren als ze zijn ingelogd. Hier kan je wel zien wat wij moeten doen om het in te voeren."
+  });
+  $('[id="uurChartInf"]').popover({
+    trigger: 'click',
+    html: true,
+    title: "Wat is dit?",
+    content: "Dit is een chart gemaakt met de <a href='https://developers.google.com/chart/' target='_null'>Google Chart API</a>. Door het gebruiken van EJS, importeren we informatie uit de database en worden dan in deze chart gezet."
   });
   
   $('#urenSubmit').on('click', function(event) {
