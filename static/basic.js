a5eec69e889b6d49e6394d571e2fb5a33c2b7941
doomtodead <jelte2410@gmail.com>
2017-11-20 21:30:03 +0100

    begin aan popup
    code is matig en heeft nog verbetering nodig!

--- static/basic.js
+++ static/basic.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   console.log("The page has loaded!");
   // Test om te zien of de pagina is geladen
-  
+
   $('#zoekknop').on('click', function(event) {
     zoek()
   });
-  
+
   $("#zoek").keyup(function(ev) {
     if (ev.which === 13) {
       zoek()
     }
   });
-  
+
   function zoek() {
     var zoek = document.getElementById('zoek').value
     // Controleren of zoek alleen maar spaties bevat
@@ -24,4 +24,23 @@ $(document).ready(function() {
       window.location.href = "/zoek/" + zoek
     }
   }
+  //pop up van code vensters
+  //var modal = document.getElementById('myPop');
+//  var btn = document.getElementsById("codeId");
+  var span = document.getElementsByClassName("close")[0];
+  var popUp = document.getElementsByClassName("popUp")[0];
+  codeId.onclick = function() {
+      popUp.style.display = "block";
+  }
+
+  close.onclick = function() {
+      popUp.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+      if (event.target == span) {
+          popUp.style.display = "none";
+      }
+  }
+
 });
