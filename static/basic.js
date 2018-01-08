261b8b0f8f0a17eafda0cf4a684095057054cbb5
doomtodead <jelte2410@gmail.com>
2018-01-09 00:27:46 +0100

    popups zijn af
    om te janken xd

--- static/basic.js
+++ static/basic.js
@@ -31,36 +31,128 @@ $(document).ready(function() {
   });
 
   //pop up van code vensters
-  var btn = document.getElementsByClassName("codeId")[0];
-  var btn1 = document.getElementsByClassName("codeIdone")[0];
-  var btn2 = document.getElementsByClassName("codeIdtwo")[0];
   var uiterlijk = document.getElementsByClassName("popUp")[0];
-  var span = document.getElementsByClassName("close")[0];
 
-  try {
-  btn.addEventListener('click', openModal);
-  } catch (err) {console.log("dit bestaat niet op deze pagina")};
-  try {
-  btn1.addEventListener('click', openModal);
-  } catch (err) {console.log("dit bestaat niet op deze pagina")};
-  try {
-  btn2.addEventListener('click', openModal);
-  } catch (err) {console.log("dit bestaat niet op deze pagina")};
-  try {
-  span.addEventListener('click', closeModal);
-  } catch (err) {console.log("dit bestaat niet op deze pagina")};
   try {
   window.addEventListener('click', clickOutside);
-  } catch (err) {console.log("dit bestaat niet op deze pagina")};
+} catch (err) {};
 
-  function openModal() {
+  $(".codeId").click(function() {
+    var codeBlok = $('.code').attr('id');
+    console.log(codeBlok);
+    if (codeBlok == "een") {
+      var codeBlokinhoud = `$.ajax({
+        url: '/login',
+        data: {
+          username: $("input[name='username']").val(),
+          password: $("input[name='password']").val()
+        },
+        type: 'POST',
+        success: function (data) {
+          console.log(data)
+          window.location.href = "/"
+        },
+        error: function (xhr, status, error) {
+          console.log(xhr.responseText)
+        }
+    });`
+    } else if (codeBlok == "drie") {
+      var codeBlokinhoud = `app.get('/', function (req, res) {
+        res.render('index.html')
+})`
+    } else if (codeBlok == "zes") {
+      var codeBlokinhoud = `var conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'pws',
+        port: 3306
+});`
+    } else if (codeBlok == "zeven") {
+      var codeBlokinhoud = `$("#raster").click(function() {
+        $(".tile").toggleClass("raster");
+          $(".rasterButton").toggleClass("paintActive");
+});`
+    }
+
+    document.getElementById("popUp-inhoud").innerHTML = codeBlokinhoud;
     uiterlijk.style.display = 'block';
-    console.log("Dit werkt");
-  }
+  });
 
-  function closeModal() {
-    uiterlijk.style.display = 'none';
-  }
+  $(".codeIdone").click(function() {
+    var codeBlokeen = $('.codeEen').attr('id');
+    console.log(codeBlokeen);
+    if (codeBlokeen == "twee") {
+      var codeBlokinhoud = `app.post('/login', function(req, res) {
+        conn.query('SELECT * FROM userinfo WHERE username = ?', req.body.username, function(err, result) {
+          if (err || result[0] == undefined) return res.status(400).send('Username en/of wachtwoord is niet correct')
+          if (req.body.password == result[0].password) {
+            req.session.user = req.body
+            req.session.logged = true
+            res.status(200).send('Succesvol ingelogd')
+          }	else {
+            res.status(400).send('Incorrect wachtwoord')
+          }
+        })
+  })`
+} else if (codeBlokeen == "vier") {
+      var codeBlokinhoud = `app.get('/zoek/:zoek', function (req, res) {
+        res.render('zoek.html', {zoek: req.params.zoek})
+  })`
+} else if (codeBlokeen == "acht") {
+      var codeBlokinhoud = `var down;
+
+      $(document).mousedown(function() {
+       down = true;
+      }).mouseup(function() {
+       down = false;
+      });
+
+      $("#zwart").click(function() {
+        $(".tile").mouseover(function(){
+          if(down) {
+            $(this).css("background-color", "black");
+          } else { $(".tile").mousedown(function(){
+            $(this).css("background-color", "black");
+           });
+          };
+        });
+  });`
+    }
+    document.getElementById("popUp-inhoud").innerHTML = codeBlokinhoud;
+    uiterlijk.style.display = 'block';
+  });
+
+  $(".codeIdtwo").click(function() {
+    var codeBloktwee = $('.codeTwee').attr('id');
+    console.log(codeBloktwee);
+    if (codeBloktwee == "vijf") {
+      var codeBlokinhoud = `&lt;&#37; res.forEach(function(res) { &#37;&gt;
+        &lt;b&gt;&lt;&#37;&#61;= res.waarde &#37;&gt;&lt;/b&gt;
+    &lt;&#37; }) &#37;&gt;`
+} else if (codeBloktwee == "negen") {
+      var codeBlokinhoud = `$("#reset").click(function() {
+       $(".tile").css("background-color", "white");
+  });`
+} else {};
+    document.getElementById("popUp-inhoud").innerHTML = codeBlokinhoud;
+    uiterlijk.style.display = 'block';
+  });
+
+  $(".codeIdthree").click(function() {
+    var codeBlokdrie = $('.codeDrie').attr('id');
+    console.log(codeBlokdrie);
+    if (codeBlokdrie == "tien") {
+      var codeBlokinhoud = `@media only screen and (max-width: 400px){
+          .tile{height: 6.25%; width: 6.25%;}
+          .kleur{height: 20px; width: 20px;}
+          #raster, #reset{height: 20px; font-size: 11px;}
+  }`
+} else {};
+
+    document.getElementById("popUp-inhoud").innerHTML = codeBlokinhoud;
+    uiterlijk.style.display = 'block';
+  });
 
   function clickOutside() {
     if (event.target == uiterlijk){
@@ -250,3 +342,19 @@ $(document).ready(function() {
   });
 
 });
+
+//teller
+var counter = {};
+var add = (function () {
+    counter = 0;
+    return function () {return counter += 1;}
+})();
+
+function telOp() {
+    document.getElementById("nummer").innerHTML = add();
+};
+
+function zetTerug() {
+     counter = 0;
+    document.getElementById("nummer").innerHTML = 0;
+};
